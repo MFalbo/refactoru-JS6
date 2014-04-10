8818b0b01385bfd420ea83073b1f1a67ef200507
Matthew Falbo <matthew.falbo@gmail.com>
2014-04-10 16:25:05 -0600

    Functional Hide-A-Form

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 $(document).on('ready', function() {
+
+	$(document).on('click', '#show-form', function(){
+		$('#input-form').toggle();
+	})
+
+	$('#input-form').submit(function(){
+
+		$('#user-name').text($('#name').val());
+		$('#bookPar').text($('#bio').val());
+		$('#bioPar').text($('#books').val());
+		$('#jsPar').text($('#libs').val());
+		
+
+		return false;
+	});
   
 });
