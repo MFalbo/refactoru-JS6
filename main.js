39246b9a1e3b5cc042004344c279ab448f75b044
Matthew Falbo <matthew.falbo@gmail.com>
2014-04-11 12:10:56 -0600

    Added auto-save function to replace form submit button.  Aesthetic updates.

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+
+// --------SOLUTION USING FORM SUBMIT BUTTON------------------
+
+// $(document).on('ready', function() {
+
+// 	$(document).on('click', '#show-form', function(){
+
+// 		$('#input-form').toggle({
+			
+// 			duration: 100,
+// 			start: function(){
+// 				if($('#show-form').text() === "Show Form"){
+// 					$('#show-form').text("Hide Form");
+// 				}
+// 				else{
+// 					$('#show-form').text("Show Form");
+// 				}
+// 			}
+// 		})
+
+// 	})
+
+// 	$('#input-form').submit(function(){
+
+// 		$('#user-name').text($('#name').val());
+// 		$('#bookPar').text($('#bio').val());
+// 		$('#bioPar').text($('#books').val());
+// 		$('#jsPar').text($('#libs').val());
+		
+
+// 		return false;
+// 	});
+  
+// });
+
+
+// -------SOLUTION WITH AUTO-SAVE INSTEAD OF SUBMIT BUTTON------------
+
 $(document).on('ready', function() {
 
 	$(document).on('click', '#show-form', function(){
@@ -17,15 +55,9 @@ $(document).on('ready', function() {
 
 	})
 
-	$('#input-form').submit(function(){
-
-		$('#user-name').text($('#name').val());
-		$('#bookPar').text($('#bio').val());
-		$('#bioPar').text($('#books').val());
-		$('#jsPar').text($('#libs').val());
-		
-
-		return false;
-	});
+		$('#name').blur(function(){$('#user-name').text($('#name').val())});
+		$('#bio').blur(function(){$('#bookPar').text($('#bio').val())});
+		$('#books').blur(function(){$('#bioPar').text($('#books').val())});
+		$('#libs').blur(function(){$('#jsPar').text($('#libs').val())});
   
 });
